eb6cea7815c63d6ab212771cfa4281bc6a9b58df
Verbal Centaur <tretti7070@wp.pl>
2021-07-17 09:15:57 +0200

    Updates

---
+++ precache-manifest.a88dad0081c27c4908b7f4ff9525b525.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a18b7abf7fa1fe31e3df",
+    "url": "/LocalWeather/static/css/main.d057ed67.chunk.css"
+  },
+  {
+    "revision": "a18b7abf7fa1fe31e3df",
+    "url": "/LocalWeather/static/js/main.e2b3057b.chunk.js"
+  },
+  {
+    "revision": "23f96be6e6a01218c285",
+    "url": "/LocalWeather/static/js/runtime~main.2de7b0bf.js"
+  },
+  {
+    "revision": "6e72dd58e738ee146a3c",
+    "url": "/LocalWeather/static/js/2.64f3549c.chunk.js"
+  },
+  {
+    "revision": "b63243e0df70bc5000dca1ccaeb00792",
+    "url": "/LocalWeather/index.html"
+  }
+];
